95e73b1320a2cfbc73df9ba8e4e58890c76ae814
Pierre LE CORRE <pierre.lecorre.free@gmail.com>
2018-11-12 10:39:27 +0100

    WOLEET-784 Sanitinzing filename on export

--- internal/app/exportReceipts.go
+++ internal/app/exportReceipts.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/kennygrant/sanitize"
 	"github.com/sirupsen/logrus"
 	"github.com/woleet/woleet-cli/pkg/api"
 	"github.com/woleet/woleet-cli/pkg/helpers"
@@ -12,7 +13,6 @@ import (
 func ExportReceipts(token string, url string, exportDirectory string, unixEpochLimit int64, exitOnError bool, logInput *logrus.Logger) {
 	log = logInput
 	client := api.GetNewClient(url, token)
-	normalizeName := strings.NewReplacer(`/`, `_`, `\`, `_`)
 
 	end := false
 
@@ -33,16 +33,21 @@ func ExportReceipts(token string, url string, exportDirectory string, unixEpochL
 			if anchor.Signature != "" {
 				currentSuffix = helpers.SuffixSignatureReceipt
 			}
-			receiptPath := exportDirectory + string(os.PathSeparator) + normalizeName.Replace(anchor.Name) + "-" + anchor.Id + currentSuffix
+
+			fields := logrus.Fields{}
+			fields["anchorID"] = anchor.Id
+			fields["anchor_Name"] = anchor.Name
+			fields["File_Name"] = sanitize.BaseName(anchor.Name) + "-" + anchor.Id + currentSuffix
+
+			receiptPath := exportDirectory + string(os.PathSeparator) + sanitize.BaseName(anchor.Name) + "-" + anchor.Id + currentSuffix
 			if _, err := os.Stat(receiptPath); !os.IsNotExist(err) {
-				log.Infof("Proof for anchor: %s named: %s is already on disk\n", anchor.Id, anchor.Name)
+				log.WithFields(fields).Infoln("Proof already on disk")
 				continue
 			}
 			if !strings.EqualFold(anchor.Status, "CONFIRMED") {
-				log.Infof("Proof for anchor: %s named: %s not available yet\n", anchor.Id, anchor.Name)
+				log.WithFields(fields).Infoln("Proof not available yet")
 				continue
 			}
-			log.Infof("Retrieving proof for anchor: %s named: %s\n", anchor.Id, anchor.Name)
 			errGetReceipt := client.GetReceiptToFile(anchor.Id, receiptPath)
 			if errGetReceipt != nil {
 				if _, err := os.Stat(receiptPath); !os.IsNotExist(err) {
@@ -54,7 +59,7 @@ func ExportReceipts(token string, url string, exportDirectory string, unixEpochL
 				errHandlerExitOnError(errAnchors, exitOnError)
 				continue
 			}
-			log.Infof("Done\n")
+			log.WithFields(fields).Infoln("Proof retrived successfully")
 		}
 	}
 }
